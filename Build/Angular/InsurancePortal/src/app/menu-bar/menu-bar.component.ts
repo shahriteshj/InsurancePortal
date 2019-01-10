f8331d695d4fa0413042694055d9be675e5eb89c
shahriteshj <43956811+shahriteshj@users.noreply.github.com>
2019-01-10 21:26:42 +0530

    .

--- Build/Angular/InsurancePortal/src/app/menu-bar/menu-bar.component.ts
+++ Build/Angular/InsurancePortal/src/app/menu-bar/menu-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedDataService } from '../service/sharedData.service';
 import { LocalStorageService } from 'ngx-webstorage';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-menu-bar',
@@ -12,10 +12,29 @@ export class MenuBarComponent implements OnInit {
 
   constructor(private _router: Router,
     private localStorage: LocalStorageService,
-    private sharedDataService: SharedDataService) { }
+    private sharedDataService: SharedDataService) {
+     
+      this._router.routeReuseStrategy.shouldReuseRoute = function(){
+        return false;
+     }
+    
+
+    this._router.events.subscribe((evt) => {
+       if (evt instanceof NavigationEnd) {
+          // trick the Router into believing it's last link wasn't previously loaded
+          this._router.navigated = false;
+          // if you need to scroll back to top, here is the right place
+          window.scrollTo(0, 0);
+       }
+   });
+  }
 
   ngOnInit() {
+    console.log(this.sharedDataService.isUserLoggedIn);
+    
+    console.log(this.sharedDataService.isOperationsUser);
     console.log(this.sharedDataService.isCustomerUser);
+    console.log(this.sharedDataService.isManagerUser);
   }
 
 }
