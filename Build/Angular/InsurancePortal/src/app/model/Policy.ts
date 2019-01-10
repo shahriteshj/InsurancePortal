40aa5885456f5c5be49c1d31047e9697da391c6d
shahriteshj <43956811+shahriteshj@users.noreply.github.com>
2019-01-10 21:25:24 +0530

    .

--- Build/Angular/InsurancePortal/src/app/model/Policy.ts
+++ Build/Angular/InsurancePortal/src/app/model/Policy.ts
@@ -5,13 +5,9 @@ export class Policy {
     policyStartDate:Date;
     policyEndDate:Date;
     status:string;
+    name?:string;
+    email?:string;
     
-    constructor(policyStartDate:Date,policyEndDate:Date,customerId?:number,customerVehicleId?:number,policyId?:number){
-        this.policyStartDate=policyStartDate;
-        this.policyEndDate=policyEndDate;
-        this.customerId=customerId;
-        this.customerVehicleId=customerVehicleId;
-        this.policyId=policyId;
-    }
+
     
 }
