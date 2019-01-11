fa656ae27529c1b25427320f9e1be58b274350c8
shahriteshj <43956811+shahriteshj@users.noreply.github.com>
2019-01-11 17:26:26 +0530

    .

--- Build/Angular/InsurancePortal/src/app/home/home.component.ts
+++ Build/Angular/InsurancePortal/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedDataService } from '../service/sharedData.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   userVerified:Boolean=true;
-  constructor() { }
+  constructor( private sharedDataService: SharedDataService) { }
 
   ngOnInit() {
+    localStorage.removeItem('currentUser');
+    localStorage.removeItem('username');
+    localStorage.removeItem('role');
+    this.sharedDataService.clear();
   }
 
 }
