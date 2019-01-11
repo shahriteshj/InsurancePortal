59419466b1e4ce407e319093ac0ac00813c03a99
shahriteshj <43956811+shahriteshj@users.noreply.github.com>
2019-01-11 11:53:10 +0530

    .

--- Build/Angular/InsurancePortal/src/app/service/sharedData.service.ts
+++ Build/Angular/InsurancePortal/src/app/service/sharedData.service.ts
@@ -13,10 +13,16 @@ export class SharedDataService{
     
 
     constructor(private localStorage: LocalStorageService){
+        console.log("shareddata service cons called");
         this.getStoredValues();
     }
 
     getStoredValues(){
+        console.log(localStorage.getItem("user"));
+        console.log(localStorage.getItem("valid"));
+        console.log(this.localStorage.retrieve("valid"));
+        console.log(this.localStorage.retrieve("user"));
+        
         this.isUserLoggedIn =  (this.localStorage.retrieve("valid")==null) ? false : this.localStorage.retrieve("valid");
         this.isAdminUser =   (this.localStorage.retrieve("user")== null) ? false : (<String>this.localStorage.retrieve("user").toLowerCase()== 'admin' ? true: false);
         this.isManagerUser =   (this.localStorage.retrieve("user")== null) ? false : (<String>this.localStorage.retrieve("user").toLowerCase()== 'manager' ? true: false);
@@ -24,6 +30,17 @@ export class SharedDataService{
         this.isCustomerUser =   (this.localStorage.retrieve("user")== null) ? false : (<String>this.localStorage.retrieve("user").toLowerCase()== 'customer' ? true: false);
         this.username = this.localStorage.retrieve("username");
     }
+
+    // getStoredValues(){
+    //     console.log(localStorage.getItem("user"));
+    //     console.log(localStorage.getItem("valid"));
+    //     this.isUserLoggedIn =  (localStorage.getItem("valid")==null) ? false :(<String> localStorage.getItem("valid").toLowerCase()== 'true' ? true: false);
+    //     this.isAdminUser =   (localStorage.getItem("user")== null) ? false : (<String>localStorage.getItem("user").toLowerCase()== 'admin' ? true: false);
+    //     this.isManagerUser =   (localStorage.getItem("user")== null) ? false : (<String>localStorage.getItem("user").toLowerCase()== 'manager' ? true: false);
+    //     this.isOperationsUser =   (localStorage.getItem("user")== null) ? false : (<String>localStorage.getItem("user").toLowerCase()== 'operations' ? true: false);
+    //     this.isCustomerUser =   (localStorage.getItem("user")== null) ? false : (<String>localStorage.getItem("user").toLowerCase()== 'customer' ? true: false);
+    //     this.username = localStorage.getItem("username");
+    // }
 
     clear(){
         this.isAdminUser = false;
